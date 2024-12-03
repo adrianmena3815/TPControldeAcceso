54becfdc06efe1dfe386e2edf2f5e0fbc3e9cf95
Adrian Mena <adriano3815@hotmail.com>
2024-12-03 07:35:04 -0300

    Mylib

--- mylib.h
+++ mylib.h
@@ -1,18 +1,34 @@
-#ifndef MY_LIB
-#define MY_LIB
-#include <stdio.h>
+
+#ifndef MYLIB_H_
+#define MYLIB_H_
+
+
+#include "LPC845.h"
+#include "GPIO_FW.h"
+#include "GPIO_SW.h"
+#include "SwitchMatrix_FW.h"
+#include "SYSCON_FW.h"
+#include "SysTick_FW.h"
 
 typedef enum {
 	espera,
-	evaluación,
+	evaluacion,
 	apertura,
 	error
 }estados_t;
 
+estados_t f_espera(int *);
+estados_t f_evaluacion(int *);
+estados_t f_apertura(int *);
+estados_t f_error(int *);
 
-estados_t f_espera(void);
-estados_t f_evaluacion(unsigned char * id);
-estados_t f_apertura(void);
-estados_t f_error(void);
+void LPC_Init(void);
+void GPIO_Init(void);
+int * leer(void);
+int comparar(int *id);
+int activarRele(void);
+void apagarRele(void);
+int activarAlarma(void);
+void apagarAlarma(void);
 
 #endif
